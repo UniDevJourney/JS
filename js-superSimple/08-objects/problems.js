1732cc2c3684f8af3a098affa2fa4820ea8a60c3
Vikas <vikasmneelagund@gmail.com>
2025-08-14 09:18:38 +0000

    objects

---
+++ js-superSimple/08-objects/problems.js
@@ -0,0 +1,46 @@
+// function comparePrice(product1, product2) {
+//     if (product1.price < product2.price) {
+//         return product1;
+//     } else if (product2.price < product1.price) {
+//         return product2;
+//     }
+// }
+// const cheaper = comparePrice({name: "hi", price: 100}, {name: "vi", price: 200})
+// console.log(`cheaper product ${cheaper.name} and price is ${cheaper.price}`)
+
+// function isSameProduct(p1, p2) {
+//     if(p1.price == p2.price && p1.name == p2.name) {
+//         // console.log(true)
+//         return true;
+//     } else {
+//         return false
+//     }
+// }
+// const compare = isSameProduct({name: 'hi', price: 200}, {name: 'hi', price: 100})
+// console.log(compare)
+// str = "alex"
+// console.log(str.repeat(3))
+
+// let age = 20;
+// if(age ==)
+
+// let random = Math.random()
+// let result = '';
+// if (random < 0.5 ){
+//     // console.log('heads');
+//     result = 'heads';
+// } else {
+//     // console.log('tails');
+//     result = 'tails';
+// }
+
+
+
+
+
+
+
+
+
+
+
